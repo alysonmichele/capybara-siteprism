e317aa63b14381991468f6518518ae7039ea1a9e
Matt Carroll <matt.carroll89@gmail.com>
2015-12-09 13:29:34 -0600

    creating a voicemail

--- capybara/spec/features/voicemail_spec.rb
+++ capybara/spec/features/voicemail_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
 
-describe "Test" do
-	it "should log in" do
+describe "Create A New VM" do
+	it "should log in and create a new voicemail app" do
 		page.driver.browser.manage.window.maximize
-		@login = LoginPage.new
+		@login = Login.new
 		@login.load
 
 		@login.login("#{$DT_PORTAL_USERNAME}", "#{$DT_PORTAL_PASSWORD}")
+
+		@vm = Voicemail.new
+		@vm.load
+
+		@vm.create_voicemail
+		@vm.vm_name("capy VM")
+		@vm.vm_expiry("12/31/2015 00:00")
+		@vm.vm_pin("0000")
+		@vm.check_read_envelope
+		@vm.check_send_email
+		@vm.vm_email_address("#{$DT_EMAIL_ADDRESS}")
+		@vm.vm_cycle_transcriptions
+		@vm.vm_select_gender
+		@vm.vm_select_gender
+		@vm.vm_save
 	end
 end
--- capybara/spec/support/pages/support_classes.rb
+++ capybara/spec/support/pages/support_classes.rb
@@ -3,7 +3,7 @@ require 'capybara'
 require 'capybara/rspec'
 require 'site_prism'
 
-class LoginPage < SitePrism::Page
+class Login < SitePrism::Page
 	set_url "/login.php"
 
 	element :username_field, "input[name='username']"
@@ -13,5 +13,54 @@ class LoginPage < SitePrism::Page
 		username_field.set(user)
 		password_field.set(pass)
 		click_button("Log In")
+	end
+end
+
+class Voicemail < SitePrism::Page
+	set_url "/vmail_main.php"
+
+	def create_voicemail
+		click_link("Create a new Voice Mail Box")
+	end
+
+	def vm_name(name)
+		fill_in "mailbox_identifier", :with => "#{name}"
+	end
+
+	def vm_expiry(expiry)
+		fill_in "vmail_vacation_expiration", :with => "#{expiry}"
+	end
+
+	def vm_pin(pin)
+		fill_in "pin", :with => "#{pin}"
+	end
+
+	def check_read_envelope
+		check("vmail_envelope")
+	end
+
+	def check_send_email
+		check("vmail_send_email")
+	end
+
+	def vm_email_address(email)
+		fill_in "email_address", :with => "#{email}"
+	end
+
+	def vm_cycle_transcriptions
+		select "Standard (automated @ $0.045/30sec)", from: "transcription_setting"
+		select "Send Audio Email, then Text Email", from: "transcription_email"
+	end
+
+	def vm_select_gender
+		select "Female", from: "gender"
+	end
+
+	def vm_select_language
+		select "Spanish", from: "ref_language_id"
+	end
+
+	def vm_save
+		click_button("Save")
 	end
 end
